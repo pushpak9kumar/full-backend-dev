6042432a0c4ab078041661abeba663f903186164
pushpak <pushpak94681@gmail.com>
2024-10-13 16:20:38 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ dotenv.config({
 
 
 connectDB()
+//promises after async
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR: ",error);
+        throw error
+        
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed  !!! ",err);
+    
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,35 @@
+//const asyncHandler = () => {}
+
+// rapper code that can be used at multiple places
+
+
+
+//method 2: promises then 
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+    export{asyncHandler}
+
+    // const asyncHandler = () => {}
+    // const asyncHandler = (func) => {}
+    // const asyncHandler = (func) =>async () => {}
+
+
+        // method 1 : try catch   
+    // const ayncHandler = (fn) => async(req,res,next) => {
+    //      try {
+    //         await fn(req, res, next)
+    //      } catch (error) {
+    //         res.status(err.code || 500).json({
+    //             success: false,
+    //             message: err.message
+    //         })
+            
+    //      }
+    // }
+
